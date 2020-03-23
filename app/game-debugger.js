006d7a0e7fde366956d3238d8378fc7e6f6fc2d6
RafalMalik <rafalmalik.info@gmail.com>
2020-03-23 02:56:12 +0100

    Working with debugger script.

--- app/game-debugger.js
+++ app/game-debugger.js
@@ -7,32 +7,33 @@ $(function() {
         'lottery'
     ];
 
-    $(document).on('click', '.send-request', function() {
+    $(document).on('click', '#send-request-btn', function() {
 
         let gameType = $(this).data('game-type');
 
-        if (!gameTypes.includes(gameType)) {
+        if (gameTypes.includes(gameType)) {
             alert('Wrong game type!');
             return;
         }
 
         $.ajax({
-            url: "http://localhost:9902/index.php/" + gameType + "/play",
+            url: "http://localhost:9902/index.php/dice/play",
             type: "POST",
-            data: JSON.stringify({
-                seed: 123,
-                range: {
-                    min: 1,
-                    max: 5
-                },
-                matrix: [
-                    [-1, -1, -1],
-                    [-1, -1, -1],
-                    [-1, -1, -1],
-                    [-1, -1, -1],
-                    [-1, -1, -1]
-                ]
-            }),
+            data: getFormData($('#debugger-request-form')),
+            // data: JSON.stringify({
+            //     seed: 123,
+            //     range: {
+            //         min: 1,
+            //         max: 5
+            //     },
+            //     matrix: [
+            //         [-1, -1, -1],
+            //         [-1, -1, -1],
+            //         [-1, -1, -1],
+            //         [-1, -1, -1],
+            //         [-1, -1, -1]
+            //     ]
+            // }),
             contentType: "application/json",
             function(gameRound) {
 
@@ -41,5 +42,19 @@ $(function() {
             }
         });
     });
+
+    function getFormData($form) {
+        var unindexed_array = $form.serializeArray();
+        console.log(unindexed_array);
+        var indexed_array = {};
+
+        $.map(unindexed_array, function(n, i) {
+            indexed_array[n['name']] = n['value'];
+        });
+
+        console.log(indexed_array);
+
+        return JSON.stringify(indexed_array);
+    }
 
 });
